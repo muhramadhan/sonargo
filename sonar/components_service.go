56c38033f2d8a3bb26a0f85a4e700cf8f8f5a789
muhramadhan <ramadhanm1998@gmail.com>
2019-09-23 10:28:17 +0700

    add component field 'branch' for branch plugin

--- sonar/components_service.go
+++ sonar/components_service.go
@@ -37,6 +37,7 @@ type Component struct {
 	UUID             string          `json:"uuid,omitempty"`
 	Version          string          `json:"version,omitempty"`
 	Visibility       string          `json:"visibility,omitempty"`
+	Branch           string          `json:"branch,omitempty"`
 }
 
 type ComponentsTreeObject struct {
